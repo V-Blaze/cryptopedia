84b68f00afce1635004822ac79a586072b6f02f9
V-Blaze <valentine6586@gmail.com>
2023-01-04 10:51:50 +0100

    Add dynamic details page html markup

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -3,9 +3,60 @@ import { useLocation } from 'react-router-dom';
 
 const Details = () => {
   const location = useLocation();
-  console.log(location);
+  const {
+    exchangeId, name, rank, percentTotalVolume, volumeUsd, tradingPairs, socket, exchangeUrl,
+  } = location.state;
   return (
-    <div>Details</div>
+    <div>
+      <div>
+        <img src={`https://logo.clearbit.com/${exchangeUrl}`} alt="logo" />
+        <div>
+          <span>
+            name:
+            {name}
+          </span>
+          <span>
+            rank
+            {rank}
+          </span>
+        </div>
+      </div>
+      <div>
+        <h3>Exchange Details</h3>
+        <ul>
+          <li>
+            <span>Trading Volum percent</span>
+            <span>
+              {percentTotalVolume}
+              %
+            </span>
+          </li>
+          <li>
+            <span>Trading Volum USD</span>
+            <span>
+              {volumeUsd}
+              $
+            </span>
+          </li>
+          <li>
+            <span>No. of Trading Pairs</span>
+            <span>{tradingPairs}</span>
+          </li>
+          <li>
+            <span>Socket</span>
+            <span>{socket}</span>
+          </li>
+          <li>
+            <span>Website</span>
+            <span>{exchangeUrl}</span>
+          </li>
+          <li>
+            <span>Exchange ID</span>
+            <span>{exchangeId}</span>
+          </li>
+        </ul>
+      </div>
+    </div>
   );
 };
 
